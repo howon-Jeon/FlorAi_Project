90d94162e035bb4b8ff7d5b672860784a235ebe4
howon-Jeon <howon0830@naver.com>
2025-04-01 16:22:37 +0900

    feat: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from "../assets/icons/logos.svg";
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-links">
+        <span>회사소개</span>
+        <span>|</span>
+        <span>이용약관</span>
+        <span>|</span>
+        <span>개인정보처리방침</span>
+        <span>|</span>
+        <span>이용안내</span>
+      </div>
+      <div className="footer-names">전호원 이석현 김성하</div>
+      <div className="footer-logo">
+        <img src={logo} alt="플로라이" />
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import MainHeader from '../components/MainHeader'
 import Banner from '../components/Banner'
 import Navbar from '../components/Navbar'
+import IntroBox from '../components/IntroBox'
+import Footer from '../components/Footer'
 
 const Main = () => {
   return (
     <div>
       <MainHeader></MainHeader>
       <Banner></Banner>
+      <IntroBox></IntroBox>
+      <Footer></Footer>
       <Navbar></Navbar>
     </div>
   )
